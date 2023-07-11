2317a40cfbe0ca67e3b006c2f0c67b455b69e5f2
unknown <Calebjwood60@gmail.com>
2023-07-11 02:57:36 -0500

    finished the timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
 var startButton = document.querySelector(".startButton")
+var timeElement = document.querySelector(".timerCount")
+var quizOpen = document.querySelector(".quizOpen")
+var quizTitle = document.querySelector(".quizTitle")
+var quizExplain = document.querySelector(".quizExplain")
 
 
 
@@ -8,6 +12,17 @@ var correctCounter = 0;
 var incorrectCounter = 0;
 var timeCounter;
 var timer;
+var chosenQuestion = ""
+var questionAmount = 5;
+var index;
+var questions = [
+    
+    "questionOne",
+    "questionTwo",
+    "questionThree",
+    "questionFour",
+    "questionFive"
+]
 
 
 
@@ -15,10 +30,38 @@ var timer;
 
 function startGame(){
     timeCounter = 60;
-    startButton.disable = true;
+    startButton.disabled = true;
     // first question function
+    StartQuestions()
     startTimer()
+    console.log()
 }
+
+
+function startTimer(){
+    timer = setInterval(function(){
+        timeCounter--;
+        timeElement.textContent = timeCounter;
+    }
+     , 1000);
+}
+
+function randomQuestion(){
+    index = Math.floor(Math.random() * questionAmount)
+    questionAmount--
+    chosenQuestion = questions[index]
+    questions.splice(index, 1)
+}
+
+function StartQuestions(){
+randomQuestion()
+
+}
+
+function questionOne(){
+quizOpen
+}
+
 
 
 
